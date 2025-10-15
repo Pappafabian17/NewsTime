c551825316988156dd5b1cdf4a2389f4b1439e7b
fabianpappa <fabian.pappa.ext@errepar.com>
2025-10-15 10:02:42 -0300

    fixing routes through vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
-import { defineConfig } from 'vite'
+import { resolve } from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   publicDir: 'src/public',
   server: {
     port: 3000
-  }
-})
+  },
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        favorites: resolve(__dirname, "favorites.html"),
+        news: resolve(__dirname, "news/index.html"),
+      },
+    },
+  },
+});
